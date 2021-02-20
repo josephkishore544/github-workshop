7f105f36556595d310db0444ad01f047c9e370d1
Joseph <jeevankishore544@gmail.com>
2021-02-20 10:16:07 +0530

    about.c modified in new branch

--- about.c
+++ about.c
@@ -10,5 +10,13 @@ int main () {
 	printf("\n");
 	for(i = n;i>0;i--)
 		printf("%d ",i);
+	//Feature1 branch addition
+	int j;
+	for(i=0;i<n;i++) {
+		for(j=0;j<i;j++) {
+			printf("*");
+		}
+		printf("\n");
+	}
 }
 
